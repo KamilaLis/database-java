c6903ae97960be85c7bda31b3c1df93a3eb24fda
Kamila <kamila.lis@outlook.com>
2017-06-16 23:47:54 +0200

    fix:init script

--- init.sh
+++ init.sh
@@ -3,6 +3,14 @@
 PASS="bd2"
 DATABASE="bd2"
 
+# Check if Java is installed
+if [ -n `which java` ]; then
+   echo "Java already installed."
+else
+   echo "Installing Java..."
+   sudo apt-get install default-jre
+fi
+
 # Check if Postgresql is installed
 UP=$(service postgresql status)
 if [ "$UP" -gt "0" ]; then
@@ -10,7 +18,7 @@ if [ "$UP" -gt "0" ]; then
   sudo apt-get update
   sudo apt-get install postgresql postgresql-contrib
 else
-  echo "Postgresql intalled."
+  echo "Postgresql already intalled."
 fi
 
 # Create database
@@ -19,6 +27,8 @@ sudo -u postgres psql --command "CREATE DATABASE $DATABASE;"
 sudo -u postgres psql --command "ALTER USER postgres WITH PASSWORD '$PASS';"
 sudo -u postgres psql $DATABASE --file tabele.sql
 
+
+echo "Database created succesfully."
 #OPCJONALNIE: Create new role
 #sudo -u postgres createuser --interactive \
 #"$USER"
