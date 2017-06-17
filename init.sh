bef43ee75e5d59adf78f01c738f1cebaac8f88eb
Kamila <kamila.lis@outlook.com>
2017-06-17 23:20:21 +0200

    fix:script

--- init.sh
+++ init.sh
@@ -1,34 +1,45 @@
 #!/bin/bash
 
+# Read variables
+#echo "Database username:"
+#read USER
+#echo "Database password:"
+#read PASS
+#echo "Database name:"
+#read DATABASE
+USER="bd2"
 PASS="bd2"
 DATABASE="bd2"
 
 # Check if Java is installed
-if [ -n `which java` ]; then
-   echo "Java already installed."
+program="java"
+condition=$(which $program 2>/dev/null | grep -v "not found" | wc -l)
+if [ $condition -eq 0 ] ; then
+    echo "$program is not installed"
+    sudo apt-get install default-jre
 else
-   echo "Installing Java..."
-   sudo apt-get install default-jre
+    echo "$program is installed"
 fi
 
+
 # Check if Postgresql is installed
-UP=$(service postgresql status)
-if [ "$UP" -gt "0" ]; then
-  echo "Installing postgresql..."
-  sudo apt-get update
-  sudo apt-get install postgresql postgresql-contrib
+program="postgresql"
+condition=$(which $program 2>/dev/null | grep -v "not found" | wc -l)
+if [ $condition -eq 0 ] ; then
+    echo "$program is not installed"
+    sudo apt-get install postgresql-9.6
 else
-  echo "Postgresql already intalled."
+    echo "$program is installed"
 fi
 
 # Create database
+service postgresql start
 echo "Creating database in postgresql..."
 sudo -u postgres psql --command "CREATE DATABASE $DATABASE;"
 sudo -u postgres psql --command "ALTER USER postgres WITH PASSWORD '$PASS';"
-sudo -u postgres psql $DATABASE --file tabele.sql
+sudo -u postgres psql $DATABASE --file init.sql
 
 
-echo "Database created succesfully."
 #OPCJONALNIE: Create new role
 #sudo -u postgres createuser --interactive \
 #"$USER"
