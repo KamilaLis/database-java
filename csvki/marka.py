f3ef4dd338cdd7b0f2209c146f28940ebd78654e
Katarzyna9527 <kasiulka9527@interia.pl>
2017-06-18 18:12:48 +0200

    dalesze  csv

--- csvki/marka.py
+++ csvki/marka.py
@@ -37,8 +37,18 @@ for i in range(len(model)):
 	file_w2.write(model[i]+'\n')
 
 file_w2.close() 
-	
 
+#Kategoria_prawa_jazdy
+file_w3 = open('kategoria_prawa_jazdy.csv', 'w')
+ 
+kategoria_prawa_jazdy = ['AM', 'A1', 'A2', 'A', 'B1', 'B', 'C1', 'C', 'D1', 'D', 'BE', 'C1E', 'CE', 'D1E', 'DE', 'T']
+
+file_w3.write('kategoria_prawa_jazdy\n')
+
+for i in range(len(kategoria_prawa_jazdy)):
+	file_w3.write(kaztegoria_prawa_jazdy[i]+'\n')
+
+file_w3.close()
 
 
 	
