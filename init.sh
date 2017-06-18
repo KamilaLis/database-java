6e7cb1d06a5412705a9bf530088e6edfcebf4b8d
Kamila <kamila.lis@outlook.com>
2017-06-18 18:36:19 +0200

    moje3

--- init.sh
+++ init.sh
@@ -41,12 +41,24 @@ sudo -u postgres psql $DATABASE --command "ALTER USER postgres WITH PASSWORD '$P
 sudo -u postgres psql $DATABASE --file init.sql
 
 # Import CSV file into table
+echo "Importing data from CSV files..."
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 sudo -u postgres psql $DATABASE --command "COPY marka(opis_marki) 
 FROM '$DIR/csvki/marka.csv' DELIMITER ',' CSV HEADER;"
 sudo -u postgres psql $DATABASE --command "COPY model(opis_modelu) 
 FROM '$DIR/csvki/model.csv' DELIMITER ',' CSV HEADER;"
-
+sudo -u postgres psql $DATABASE --command "COPY typ_pojazdu(opis_typ) 
+FROM '$DIR/csvki/typ_pojazdu.csv' DELIMITER ',' CSV HEADER;"
+sudo -u postgres psql $DATABASE --command "COPY kategoria_prawa_jazdy(opis_kat) 
+FROM '$DIR/csvki/kategoria_prawa_jazdy.csv' DELIMITER ',' CSV HEADER;"
+sudo -u postgres psql $DATABASE --command "COPY rodzaj_ladunku(opis_rodzaju) 
+FROM '$DIR/csvki/rodzaj_ladunku.csv' DELIMITER ',' CSV HEADER;"
+sudo -u postgres psql $DATABASE --command "COPY czesc_samochodowa(opis_czesci) 
+FROM '$DIR/csvki/czesc_samochodowa.csv' DELIMITER ',' CSV HEADER;"
+sudo -u postgres psql $DATABASE --command "COPY rodzaj_materialu(opis_rodzaju) 
+FROM '$DIR/csvki/rodzaj_materialu.csv' DELIMITER ',' CSV HEADER;"
+sudo -u postgres psql $DATABASE --command "COPY czynnosc(opis_czynnosci) 
+FROM '$DIR/csvki/czynnosc.csv' DELIMITER ',' CSV HEADER;"
 
 #OPCJONALNIE: Create new role
 #sudo -u postgres createuser --interactive \
